d60ca2f2a778edce8b119aa9019136235bf3015e
root <root@localhost>
2024-12-31 22:34:32 +0530

    auto publish 1735664671360

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// Add react app code here
+class Hello extends React.Component {
+  render() {
+    return (<div className="back">
+    <div className="happy"><h1>HAPPY NEW YEAR </h1>
+    <div className="new">
+    <div className="num"><h1>2 </h1></div>
+    <div className="nem"><h1>0 </h1></div>
+    <div className="net"><h1>2 </h1></div>
+    <div className="not"><h1>4 </h1></div>
+    <div className="nor"><h1>5 </h1></div>
+    </div>
+    </div>
+    </div>)
+  }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('app'))
+    
